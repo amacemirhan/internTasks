22f8ec5d276a1875582cc2b94f23c15a59fed977
Mehmed Emirhan Amaç <55780903+amacemirhan@users.noreply.github.com>
2022-07-18 17:27:22 +0300

    Add files via upload
    Script that lists devices with no signal for more than 2 hours

---
+++ task.py
@@ -0,0 +1,30 @@
+
+from os import access
+import requests
+import json
+import datetime
+todo = {
+  "email": "admin",
+  "password": "admin"
+}
+r=requests.post("http://37.148.212.112:8080/api/internal/login",json=todo)
+token=r.json()['jwt']
+myDict = {
+
+}
+appData=requests.get("http://37.148.212.112:8080/api/devices?limit=15&applicationID=13",headers={"Grpc-Metadata-Authorization":token})
+for i in range(int(appData.json()['totalCount'])):
+   myDict[appData.json()['result'][i]['name']] = appData.json()['result'][i]['lastSeenAt']
+for key in myDict:
+    try:
+        date_time_obj = datetime.datetime.strptime(myDict[key], '%Y-%m-%dT%X.%fZ')
+        diff = datetime.datetime.now()-date_time_obj
+        diff_in_hours = diff.total_seconds() / 3600
+        if(diff_in_hours>2):
+            print(key + " ->  {hour:.2f} hours ago".format(hour=diff_in_hours))
+    except TypeError:
+        print(key + " -> last time is unknown")
+    
+
+
+
